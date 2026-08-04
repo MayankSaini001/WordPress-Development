7b2c01a0e3e8bd8d987df19239925b6a2089bc8c
MayankSaini001 <mayanksaini200@gmail.com>
2026-08-04 15:49:11 +0530

    Updated WordPress theme

--- wp-content/themes/Custom_theme/functions.php
+++ wp-content/themes/Custom_theme/functions.php
@@ -1,16 +1,47 @@
 <?php
 
+// Theme Setup
 function custom_theme_setup() {
+
+    // Dynamic Title
     add_theme_support('title-tag');
+
+    // Featured Image Support
     add_theme_support('post-thumbnails');
+
+    // Custom Logo Support
     add_theme_support('custom-logo');
-    add_theme_support('menus');
+
+    // Register Navigation Menu
+    register_nav_menus(array(
+        'primary-menu' => 'Primary Menu',
+    ));
 }
 
 add_action('after_setup_theme', 'custom_theme_setup');
 
+
+// Load CSS & JS
 function custom_theme_scripts() {
-    wp_enqueue_style('theme-style', get_stylesheet_uri());
+
+    // Main CSS
+    wp_enqueue_style(
+        'theme-style',
+        get_stylesheet_uri()
+    );
+
+    // Agar baad me JS add karna ho to ye uncomment kar dena
+
+    /*
+    wp_enqueue_script(
+        'theme-script',
+        get_template_directory_uri() . '/assets/js/main.js',
+        array(),
+        '1.0',
+        true
+    );
+    */
+
 }
 
 add_action('wp_enqueue_scripts', 'custom_theme_scripts');
--- wp-content/themes/Custom_theme/header.php
+++ wp-content/themes/Custom_theme/header.php
@@ -6,3 +6,22 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<header>
+
+    <?php the_custom_logo(); ?>
+
+    <h1><?php bloginfo('name'); ?></h1>
+
+    <p><?php bloginfo('description'); ?></p>
+
+    <nav>
+        <?php
+        wp_nav_menu(array(
+            'theme_location' => 'primary-menu',
+            'menu_class'     => 'main-menu',
+        ));
+        ?>
+    </nav>
+
+</header>
