926144b5028b27b34210605af6234e54790d1bac
MayankSaini001 <mayanksaini200@gmail.com>
2026-08-06 11:06:07 +0530

    Create custom plugin

---
+++ wp-content/plugins/custom-plugin/form.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name : Custom plugin
+Description : Collect the information of the students
+Author : Mayank Saini
+Version : 1.0
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; 
+}
+
+function cp_create_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'cp_students';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        name tinytext NOT NULL,
+        email varchar(100) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
+register_activation_hook( __FILE__, 'cp_create_table' );
